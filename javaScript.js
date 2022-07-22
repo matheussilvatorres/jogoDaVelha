9baf629e131a78b6ed4c0e58b52d2738379eb43e
Matheus Torres <matheustorres201900@gmail.com>
2022-07-22 01:54:36 -0300

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,5 @@
 localStorage.setItem('turn', 'x');
-localStorage.setItem('game', '---------')
+localStorage.setItem('game', '---------');
 
 function fieldClick(id){
     var turn = localStorage.getItem('turn');
@@ -16,7 +16,9 @@ function fieldClick(id){
         game = game.substring(0, Number(id) - 1) + turn + game.substring(Number(id));
         localStorage.setItem('game', game);
         var winner = verifyWinner(game);
+        setTimeout(function (){
         if (winner) window.alert(`${winner} venceu`);
+        }, 100)
     }
 }
 
@@ -29,21 +31,22 @@ function verifyWinner(game){
             winner = verifyDiagonal(game);
         }
     }
+    return winner; 
 }
 
 function verifyRow(game){
     // primeira linha
-    if(game[0] != '-' && game[0] == [1] && game[0] == game[2]){
+    if(game[0] != '-' && game[0] == game[1] && game[0] == game[2]){
         if(game[0] == 'x') return 'x';
         else return 'o';
     }
     // segunda linha
-    if(game[3] != '-' && game[3] == [4] && game[3] == game[5]){
+    if(game[3] != '-' && game[3] == game[4] && game[3] == game[5]){
         if(game[3] == 'x') return 'x';
         else return 'o';
-    }
+    } 
     // terceira linha
-    if(game[6] != '-' && game[6] == [7] && game[6] == game[8]){
+    if(game[6] != '-' && game[6] == game[7] && game[6] == game[8]){
         if(game[6] == 'x') return 'x';
         else return 'o';
     }
@@ -52,17 +55,17 @@ function verifyRow(game){
 
 function verifyCol(game){
         // primeira coluna
-        if(game[0] != '-' && game[0] == [3] && game[0] == game[6]){
+        if(game[0] != '-' && game[0] == game[3] && game[0] == game[6]){
             if(game[0] == 'x') return 'x';
             else return 'o';
         }
         // segunda coluna
-        if(game[1] != '-' && game[1] == [4] && game[1] == game[7]){
+        if(game[1] != '-' && game[1] == game[4] && game[1] == game[7]){
             if(game[3] == 'x') return 'x';
             else return 'o';
         }
         // terceira coluna
-        if(game[2] != '-' && game[2] == [5] && game[2] == game[8]){
+        if(game[2] != '-' && game[2] == game[5] && game[2] == game[8]){
             if(game[2] == 'x') return 'x';
             else return 'o';
         }
@@ -71,13 +74,13 @@ function verifyCol(game){
 
 function verifyDiagonal(game){
     // primeira diagonal
-    if(game[0] != '-' && game[0] == [4] && game[0] == game[8]){
+    if(game[0] != '-' && game[0] == game[4] && game[0] == game[8]){
         if(game[0] == 'x') return 'x';
         else return 'o';
     }
 
     // segunda diagonal
-    if(game[2] != '-' && game[2] == [4] && game[2] == game[6]){
+    if(game[2] != '-' && game[2] == game[4] && game[2] == game[6]){
         if(game[2] == 'x') return 'x';
         else return 'o';
     }
